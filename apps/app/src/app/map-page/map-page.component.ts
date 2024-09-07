24eb352040e3aa33f42f1af3b857429f7dcde278
Onur Bicer <onur.bicer@lapisit.com.au>
2024-09-07 15:37:22 +1000

    Added map

--- apps/app/src/app/map-page/map-page.component.ts
+++ apps/app/src/app/map-page/map-page.component.ts
@@ -1,14 +1,35 @@
-import {Component, signal} from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {MatDrawer, MatDrawerContainer, MatDrawerContent, MatSidenavContainer} from "@angular/material/sidenav";
+import {
+  MatDrawer,
+  MatDrawerContainer,
+  MatDrawerContent,
+} from '@angular/material/sidenav';
+import { LeafletModule } from '@bluehalo/ngx-leaflet';
+import { latLng, tileLayer } from 'leaflet';
 
 @Component({
   selector: 'app-map-page',
   standalone: true,
-  imports: [CommonModule,  MatDrawerContainer, MatDrawer, MatDrawerContent],
+  imports: [
+    CommonModule,
+    MatDrawerContainer,
+    MatDrawer,
+    MatDrawerContent,
+    LeafletModule,
+  ],
   templateUrl: './map-page.component.html',
   styleUrl: './map-page.component.css',
 })
 export class MapPageComponent {
-
+  options = {
+    layers: [
+      tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 18,
+        attribution: '...',
+      }),
+    ],
+    zoom: 10,
+    center: latLng(-37.8136, 144.9631),
+  };
 }
