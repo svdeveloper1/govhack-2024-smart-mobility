df339fc6ca409f3544846387a097d77bbe60238c
Onur Bicer <onur.bicer@lapisit.com.au>
2024-09-07 15:29:25 +1000

    Prepared drawer

--- apps/app/src/app/map-page/map-page.component.ts
+++ apps/app/src/app/map-page/map-page.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import {Component, signal} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatDrawer, MatDrawerContainer, MatDrawerContent, MatSidenavContainer} from "@angular/material/sidenav";
 
 @Component({
   selector: 'app-map-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,  MatDrawerContainer, MatDrawer, MatDrawerContent],
   templateUrl: './map-page.component.html',
   styleUrl: './map-page.component.css',
 })
-export class MapPageComponent {}
+export class MapPageComponent {
+
+}
